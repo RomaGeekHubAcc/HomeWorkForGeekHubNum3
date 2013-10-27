c1668c2371eac553fee97872c69d65f362d616b0
Roman_Rybachenko <rom_rybachenko@ukr.net>
2013-10-27 10:02:12 +0200

    Initial Commit

---
+++ HWforgeekHub3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HWforgeekHub3
+//
+//  Created by Roma on 27.10.13.
+//  Copyright (c) 2013 Roma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HWforgeekHub3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HWforgeekHub3
+//
+//  Created by Roma on 27.10.13.
+//  Copyright (c) 2013 Roma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
